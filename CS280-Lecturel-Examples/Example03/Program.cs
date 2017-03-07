89b9fd2fa96c7ebbdb601d79af85cf2a36897142
FrankLikuohao <frank.likuohao@gmail.com>
2017-03-07 20:48:15 +0800

    文字型態教學

---
+++ CS280-Lecturel-Examples/Example03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string sq1 =
+                @"select * from table
+                  where id >1000";
+            string xm1 =
+                 @"<Pokemon>
+                    <NationalNo><#001></NationalNo>
+                    <Name><Bulbsaur></Name>
+                    </Pokemon>";
+
+            string xm2 =
+                  @"Pokemon ={
+                    NationalNo: '#001',
+                    Name: Bulbsaur
+                    }";
+
+            Console.WriteLine(sq1);
+            Console.WriteLine(xm1);
+            Console.WriteLine(xm2);
+        }
+    }
+}
